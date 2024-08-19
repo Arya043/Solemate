56c37b5c3b910db57b6cbecce8d2806743ea7c95
Arya043 <aryasutar621@gmail.com>
2024-08-19 19:59:41 +0530

    updatet the all css and home page for obile woking is not done

---
+++ JS/iconedrop.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menu = document.querySelector('ul');
+
+    menuIcon.addEventListener('click', function() {
+        menu.classList.toggle('show');
+        this.style.transform = menu.classList.contains('show') ? 'rotate(90deg)' : 'rotate(0)';
+    });
+});
